4c1d213d9df66c20bb3095a42f71965279993d50
hernanastorino <hastorino@correo.um.edu.uy>
2025-10-13 08:22:21 -0300

    initial push

---
+++ src/main/java/um/edu/uy/PizzUmBurgUmApplication.java
@@ -0,0 +1,13 @@
+package um.edu.uy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzUmBurgUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzUmBurgUmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/um/edu/uy/PizzUmBurgUmApplicationTests.java
@@ -0,0 +1,13 @@
+package um.edu.uy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzUmBurgUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
