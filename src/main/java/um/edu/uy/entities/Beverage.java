bc4e24030bd300c33905a58cc000fbff0783afd0
WilliamHobbins <willyhobbins@gmail.com>
2025-10-24 09:23:25 -0300

    creación de entidades del MER

--- src/main/java/um/edu/uy/entities/Beverage.java
+++ src/main/java/um/edu/uy/entities/Beverage.java
@@ -1,4 +1,25 @@
 package um.edu.uy.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Beverage")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Beverage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BeverageId")
+    private long id;
+
+    @Column(name = "BeverageName")
+    private String name;
+
+    @Column(name = "BeveragePrice")
+    private int price;
 }
--- src/main/java/um/edu/uy/entities/Burger.java
+++ src/main/java/um/edu/uy/entities/Burger.java
@@ -1,4 +1,29 @@
 package um.edu.uy.entities;
 
-public class Burger {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Burger")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Burger extends Creation {
+    @Column(name = "BurgerMeat")
+    private String meat;
+
+    @Column(name = "BurgeramountOfMeats")
+    private int amountOfMeats;
+
+    @Column(name = "BurgerBread")
+    private String bread;
+
+    @OneToMany(mappedBy = "burger", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Sauce> sauces = new ArrayList<>();
 }
