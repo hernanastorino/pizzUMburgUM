502a54f96fcba0e1f622a97aacba704b85451187
Vangeloro <vangeloro@correo.um.edu.uy>
2025-10-26 20:31:17 -0300

    Integrar frontend con Spring Boot y configurar routing de páginas

---
+++ src/main/java/um/edu/uy/PublicController.java
@@ -0,0 +1,33 @@
+package um.edu.uy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublicController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/sesion")
+    public String sesion() {
+        return "sesion";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/testpage")
+    public String testPage() {
+        return "test";
+    }
+}
